505b6fa588f66a7af11ec2c9f049cbb6e101e32d
Chris Garcia <o3fingers@gmail.com>
2015-05-03 13:08:25 -0500

    More robust classes, form field generation for adding churches.

--- spreet.js
+++ spreet.js
@@ -104,12 +104,93 @@ var Datum = Class.extend({
         this.help = 'Any text will do';
         this.fieldType = 'text';
         this.validator = new RegExp();
+        this.choices = [];
         data = (typeof data == 'object') ? data : {};
         for (key in data) {
             if (typeof this[key] != 'undefined') {
                 this[key] = data[key];
             }
         }
+    },
+    getField: function() {
+        var field = {
+            label: this.label,
+            type: this.fieldType,
+            placeholder: this.placeholder,
+            help: this.help,
+            min: this.min,
+            max: typeof this.max == 'number' && this.max || null,
+            value: this.value
+        }
+        switch(this.fieldType) {
+            case 'text':
+                field.input = true;
+                break;
+            case 'number':
+                field.input = true;
+                break;
+            case 'textarea':
+                field.textarea = true;
+                break;
+        }
+        return field;
+    }
+});
+
+var DatumChoice = Datum.extend({
+    init: function(data) {
+        data = (typeof data == 'object') ? data : {};
+        var defaultData = {
+            allowedType: 'string',
+            label: 'Choice',
+            help: 'Pick an item from the list',
+            fieldType: 'select',
+            placeholder: 'Choose one',
+            choices: [
+                {
+                    key: 'a',
+                    value: 'A'
+                },
+                {
+                    key: 'b',
+                    value: 'B'
+                },
+                {
+                    key: 'c',
+                    value: 'C'
+                },
+                {
+                    key: 'd',
+                    value: 'D'
+                },
+                {
+                    key: 'e',
+                    value: 'All of the above'
+                }
+            ],
+            allowOther: false
+        };
+        for (key in defaultData) {
+            if (typeof data[key] == 'undefined') {
+                data[key] = defaultData[key];
+            }
+        }
+        this._super(data);
+        delete this.max;
+        delete this.min;
+        delete this.validator;
+    },
+    getField: function() {
+        var field = {
+            label: this.label,
+            type: this.fieldType,
+            help: this.help,
+            value: this.value,
+            choices: this.choices,
+            select: true,
+            placeholder: this.placeholder
+        };
+        return field;
     }
 });
 
@@ -121,17 +202,17 @@ var DatumText = Datum.extend({
 
 var DatumNumber = Datum.extend({
     init: function(data) {
+        data = (typeof data == 'object') ? data : {};
         var defaultData = {
             allowedType: 'number',
             max: 'none',
             label: 'Number',
-            placeholder: 'Type a number here',
+            placeholder: '0',
             help: 'Any number will do',
             fieldType: 'number',
             validator: 'minMax',
             value: 0
-        }
-        data = (typeof data == 'object') ? data : {};
+        };
         for (key in defaultData) {
             if (typeof data[key] == 'undefined') {
                 data[key] = defaultData[key];
@@ -141,20 +222,57 @@ var DatumNumber = Datum.extend({
     }
 });
 
+Churches = new Meteor.Collection('churches');
+
+Faiths = new Meteor.Collection('faiths');
+
+Denominations = new Meteor.Collection('denominations');
+
+WorshipStyles = new Meteor.Collection('worshipStyles');
+
+MessageStyles = new Meteor.Collection('messageStyles');
+
 var Church = Class.extend({
     init: function(data) {
         this.id = new DatumText({value: newUuid()}); // Auto
         this.name = new DatumText({label: 'Name', placeholder: 'Example: First Church', help: '', required: true}); // Create form
-        this.description = new DatumText(); // Create form
-        this.address = new DatumText(); // Create form
-        this.city = new DatumText(); // Create form
-        this.state = new DatumText(); // Create form
-        this.zip = new DatumText(); // Create form
-        this.size = new DatumNumber(); // Create form
-        this.faith = new DatumText(); // Create form
-        this.denomination = new DatumText(); // Create form
-        this.worshpStyle = new DatumText(); // Create form
-        this.messageStyle = new DatumText(); // Create form
+        this.description = new DatumText({label: 'Description', placeholder: 'Example: Quiet, older church building with a yound congregation.', help: '', required: true, fieldType: 'textarea'}); // Create form
+        this.address = new DatumText({label: 'Address', placeholder: '123 Main St.', help: '', required: true}); // Create form
+        this.city = new DatumText({label: 'City', placeholder: 'New York', help: '', required: true}); // Create form
+        this.state = new DatumText({label: 'State', placeholder: 'NY', help: '', required: true, min: 2, max: 2}); // Create form
+        this.zip = new DatumText({label: 'ZIP', placeholder: '000000', help: '', required: true, min: 5}); // Create form
+        this.size = new DatumChoice({label: 'Size', help: 'How many members attend in a typical weekend?',
+            choices: [
+                {
+                    key: 'xxs',
+                    value: '0 - 49 people'
+                },
+                {
+                    key: 'xs',
+                    value: '50 - 199 people'
+                },
+                {
+                    key: 'sm',
+                    value: '200 - 499 people'
+                },
+                {
+                    key: 'md',
+                    value: '500 - 999 people'
+                },
+                {
+                    key: 'lg',
+                    value: '1,000 - 4,999 people'
+                },
+                {
+                    key: 'xl',
+                    value: '5,000+ people'
+                }
+            ]
+        }); // Create form
+        this.faith = new DatumText({label: 'Faith', placeholder: 'Christian', help: '', required: true, value: 'Christian'}); // Create form
+        this.denomination = new DatumText({label: 'Denomination', placeholder: 'Methodist', help: '', required: true}); // Create form
+        this.worshipStyle = new DatumText({label: 'Worship style', placeholder: 'Contemporary', help: '', required: false}); // Create form
+        this.messageStyle = new DatumText({label: 'Message style', placeholder: 'Instructional', help: '', required: false}); // Create form
         this.relatedChurches = []; // Create form
         this.integrations = {}; // Sub-form
         this.services = {}; // Sub-form
@@ -166,6 +284,18 @@ var Church = Class.extend({
         this.averageRating = 0; // Computed
         this.testimonials = []; // Collected later
         this.media = []; // Collected later
+        this.adminFields = [
+            this.name,
+            this.description,
+            this.address,
+            this.city,
+            this.state,
+            this.zip,
+            this.size,
+            this.denomination,
+            this.worshipStyle,
+            this.messageStyle
+        ];
         data = (typeof data == 'object') ? data : {};
         for (key in data) {
             if (typeof this[key] != 'undefined') {
@@ -177,55 +307,21 @@ var Church = Class.extend({
     },
     getForm: function() {
         var form = {
-            firstLabel: this.name.label,
-            firstType: this.name.fieldType,
-            firstPlaceholder: this.name.placeholder,
-            firstHelp: this.name.firstHelp,
-            firstMin: this.name.min,
-            firstMax: typeof this.name.max == 'number' && this.name.max || null,
-            firstValidator: this.name.validator
+                forms: [
+                {
+                    type: 'fieldset',
+                    legend: 'Add a church',
+                    fields: []
+                }
+            ]
         };
+        for (var i = 0; i < this.adminFields.length; i++) {
+            form.forms[0].fields.push(this.adminFields[i].getField());
+        }
+        console.log(form);
         return form;
     }
 });
-
-// var sampleChurch = [
-//   'id1': {
-//       'id': 'id1', // Auto
-//       'name': 'Shoreline Church', // Free text
-//       'description': '', // Free text
-//       'integrations': {
-//         'google': { // Pulled from Google Maps integration
-//             'place': '' // string
-//         },
-//         'yelp': {
-//             // ?
-//         },
-//         'facebook': {
-//             'page': '', // Free url
-//             'events': '', // Facebook stuff?
-//         }
-//       },
-//       'address': '2010 Burnet Rd', // Free text
-//       'city': 'Austin', // Free text
-//       'state': 'TX', // Free text
-//       'zip': '78754', // Free text
-//       'services': [], // Sub-form
-//       'size': 0, // Free text
-//       'worshipStyle': '', // ??
-//       'messageStyle': '', // ??
-//       'denomination': 'non-denominational', // ??
-//       'amenities': [], // Multi-text
-//       'tags': [], // Multi-text
-//       'childcare': [], // Computed: "some service" / "all services" / "most services" / "none"
-//       'phone': '', // Free tel
-//       'website': '', // Free url
-//       'averageRating': 0, // Computed
-//       'testimonials': [], // Sub-form of testimonials,
-//       'media': [], // Sub-form of image or video
-//       'relatedChurches': [] // Reference to other church objects
-//   }
-// ];
 
 if (Meteor.isClient) {
 
@@ -252,31 +348,38 @@ if (Meteor.isClient) {
         }
     });
 
+    // View - admin screen
+    Template.admin.helpers({
+        addChurchFormVisible: function() {
+            return Session.get('addChurchFormVisible')
+        }
+    });
+    Template.admin.events({
+        'click button[data-click="showAddChurchForm"]': function() {
+            Session.set('addChurchFormVisible', true);
+        },
+        'click button[data-click="hideAddChurchForm"]': function() {
+            Session.set('addChurchFormVisible', false);
+        },
+        'click button[data-click="clearChurchFormVisible"]': function() {
+
+        }
+    });
+
+    // View - add church form
     var inputChurch = new Church();
     var addChurchForm = inputChurch.getForm();
-    console.log(addChurchForm);
+    Template.addChurchForm.helpers({
+        denominations: function() {
+            return Denominations.find().fetch().map(function(it) {return it.name});
+        }
+    });
 
     Template.addChurchForm.helpers(addChurchForm);
+    // Template.addChurchForm.events(addChurchForm);
 
-    // inputChurch = new Church();
-
-    // console.log(inputChurch);
-
-    // Template.addChurchForm.helpers(function() {
-    //     return {
-    //     firstLabel: inputChurch.name.label,
-    //     firstFieldType: inputChurch.name.fieldType,
-    //     firstPlaceholder: inputChurch.name.placeholder,
-    //     firstHelp: inputChurch.name.firstHelp,
-    //     firstMin: inputChurch.name.min,
-    //     firstMax: typeof inputChurch.name.max == 'number' && inputChurch.name.max || null,
-    //     firstValidator: inputChurch.name.validator
-    //     };
-    // });
-
+    // Sample church
     var firstChurch = new Church({name: 'First Church'});
-
-    // console.log(firstChurch);
 
     Router.route('/admin', function () {
         Meta.setTitle("Admin");
@@ -286,10 +389,46 @@ if (Meteor.isClient) {
     Router.route('/', function () {
         this.render('home');
     });
+
+    function show(target) {
+        target.visible = true;
+    }
+    function hide(target) {
+        target.visible = false;
+    }
+    function toggle(target) {
+        target.visible = !target.visible;
+    }
 }
 
 if (Meteor.isServer) {
     Meteor.startup(function () {
-    // code to run on server at startup
+        // code to run on server at startup
+
+        // Lists from which to autocomplete
+        // Denominations = new Meteor.Collection('denominations');
+        var denominations = [
+            {name: 'Catholic - Latin Catholic'},
+            {name: 'Catholic - Eastern Catholic'},
+            {name: 'Catholic - Other Catholic'},
+            {name: 'Baptist'},
+            {name: 'Lutheran'},
+            {name: 'Presbyterian'},
+            {name: 'Methodist'},
+            {name: 'Pentecostal'},
+            {name: 'Seventh-day Adventist'},
+            {name: 'New Apostolic'},
+            {name: 'Russian Orthodox'},
+            {name: 'Romanian Orthodox'},
+            {name: 'Church of Greece'},
+            {name: 'Episcopal'},
+            {name: 'Mormon'},
+            {name: 'Jehovah\'s Witness'},
+            {name: 'Unitarian'},
+            {name: 'Non-denominational'},
+        ];
+        for (var i = 0; i < denominations.length; i++) {
+            Denominations.insert(denominations[i]);
+        }
     });
 }
